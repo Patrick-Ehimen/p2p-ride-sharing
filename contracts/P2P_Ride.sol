fd5409654ef180bc5ecb2eb2108ec7985189bfe8
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-03-05 21:51:54 +0100

    Update P2P_Ride.sol

--- contracts/P2P_Ride.sol
+++ contracts/P2P_Ride.sol
@@ -7,7 +7,7 @@ contract P2PRide is Driver {
     address public owner;
     struct Ride {
         address passenger;
-        string desination;
+        string destination;
         string pickupLocation;
         bool isComplete;
         bool booked;
@@ -15,6 +15,12 @@ contract P2PRide is Driver {
     uint256 price; // rideId => Ride
 
     Ride[] public rides;
+
+    event RideBooked(
+        address indexed passenger,
+        string destination,
+        string pickupLocation
+    );
 
     constructor() {
         owner = msg.sender;
@@ -28,17 +34,19 @@ contract P2PRide is Driver {
     function bookRide(
         string memory _destination,
         string memory _pickupLocation
-    ) public {
-        rides.push(
-            Ride(msg.sender, _destination, _pickupLocation, false, false)
-        );
-        Ride memory newRide = rides.push();
-        require(newRide.booked == false, "Ride already booked");
-        newRide.booked = true;
-        newRide.passenger = msg.sender;
-        newRide.desination = _destination;
-        newRide.pickupLocation = _pickupLocation;
-        newRide.isComplete = false;
+    ) public payable {
+        require(msg.sender != owner, "Owner cannot book a ride");
+        require(msg.value == price, "Insufficient funds");
+
+        // Ride memory newRide = rides.push();
+        // require(newRide.booked == false, "Ride already booked");
+        // newRide.booked = true;
+        // newRide.passenger = msg.sender;
+        // newRide.desination = _destination;
+        // newRide.pickupLocation = _pickupLocation;
+        // newRide.isComplete = false;
+
+        emit RideBooked(msg.sender, _destination, _pickupLocation);
     }
 
     function getRidePrice() public {}
