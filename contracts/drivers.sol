2058eb491dd91b4f19edef4d02d279b5d917ed08
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-03-08 23:34:51 +0100

    Update drivers.sol

--- contracts/drivers.sol
+++ contracts/drivers.sol
@@ -8,6 +8,7 @@ contract Driver {
     uint public numOfDrivers;
     address[] public drivers;
     uint public immutable registrationFee;
+    address public owner;
 
     struct DriverDetails {
         string name;
@@ -31,6 +32,7 @@ contract Driver {
 
     constructor() {
         registrationFee = 0.1 ether;
+        owner = msg.sender;
     }
 
     function driverSignup(
@@ -39,6 +41,7 @@ contract Driver {
         string memory _carNumber
     ) public payable {
         require(msg.value == registrationFee, "Registration fee not paid");
+        require(msg.sender != owner, "Owner cannot be a driver");
         require(!listOfDrivers[msg.sender], "Driver already exists");
         addDriver();
         DriverDetails memory newDriver = DriverDetails({
@@ -49,15 +52,17 @@ contract Driver {
             totalRides: 0,
             totalEarnings: 0
         });
+
         driversDetails[msg.sender] = newDriver;
         emit DriverSignup(msg.sender, _name, _carModel, _carNumber);
     }
 
-    function addDriver() internal {
+    function addDriver() public {
         require(!listOfDrivers[msg.sender], "Driver already exists");
         drivers.push(msg.sender);
         listOfDrivers[msg.sender] = true;
         numOfDrivers++;
+
         emit DriverAdded(msg.sender);
     }
 }
