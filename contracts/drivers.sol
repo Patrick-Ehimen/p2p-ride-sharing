60e6f9a7cfb863db976baa0e12dd10aac3389eea
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-03-09 00:42:21 +0100

    Update drivers.sol

--- contracts/drivers.sol
+++ contracts/drivers.sol
@@ -72,6 +72,20 @@ contract Driver {
         emit DriverAdded(msg.sender);
     }
 
+    function acceptRide() public {
+        require(msg.sender != owner, "Owner cannot accept a ride");
+        require(rideCount > 0, "No rides to accept");
+
+        // Iterate through the rides array to find the ride that matches the passenger
+        for (uint256 i = 0; i < rides.length; i++) {
+            // If the ride is booked and the passenger matches the sender, accept the ride
+            if (rides[i].booked == true && rides[i].passenger == msg.sender) {
+                rides[i].booked = false;
+                break;
+            }
+        }
+    }
+
     function withdraw() public onlyOwner {
         payable(msg.sender).transfer(balance);
         balance = 0;
