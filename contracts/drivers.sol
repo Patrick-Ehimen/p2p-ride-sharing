321e667ccfd13f863cf8e1180c5af2cffec27490
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-03-08 22:20:06 +0100

    Update drivers.sol

--- contracts/drivers.sol
+++ contracts/drivers.sol
@@ -2,4 +2,20 @@
 
 pragma solidity ^0.8.0;
 
-contract Driver {}
+contract Driver {
+    uint public numOfDrivers;
+    address[] public drivers;
+
+    struct DriverDetails {
+        string name;
+        string carModel;
+        string carNumber;
+        uint256 rating;
+        uint256 totalRides;
+        uint256 totalEarnings;
+    }
+
+    mapping(address => bool) public driversAddress;
+
+    event DriverAdded(address indexed driver);
+}
