4eb6d258966b200f43483a40b23f15b7af123cc4
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-03-09 11:00:08 +0100

    Update drivers.sol

--- contracts/drivers.sol
+++ contracts/drivers.sol
@@ -62,7 +62,7 @@ contract Driver {
         emit DriverSignup(msg.sender, _name, _carModel, _carNumber);
     }
 
-    function addDriver() public {
+    function addDriver() internal {
         require(msg.sender != owner, "Owner cannot be a driver");
         require(!driversExist[msg.sender], "Driver already exists");
         drivers.push(msg.sender);
@@ -70,20 +70,6 @@ contract Driver {
         numOfDrivers++;
 
         emit DriverAdded(msg.sender);
-    }
-
-    function acceptRide() public {
-        require(msg.sender != owner, "Owner cannot accept a ride");
-        require(rideCount > 0, "No rides to accept");
-
-        // Iterate through the rides array to find the ride that matches the passenger
-        for (uint256 i = 0; i < rides.length; i++) {
-            // If the ride is booked and the passenger matches the sender, accept the ride
-            if (rides[i].booked == true && rides[i].passenger == msg.sender) {
-                rides[i].booked = false;
-                break;
-            }
-        }
     }
 
     function withdraw() public onlyOwner {
