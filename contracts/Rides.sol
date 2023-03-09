33e990e08f6cc2c4c200de1038d5a4321d030587
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-03-09 10:59:59 +0100

    Update Rides.sol

--- contracts/Rides.sol
+++ contracts/Rides.sol
@@ -6,17 +6,41 @@ import "./Drivers.sol";
 pragma solidity ^0.8.0;
 
 contract Rides is P2PRide {
-    function acceptRide() public {
-        require(msg.sender != owner, "Owner cannot accept a ride");
-        require(rideCount > 0, "No rides to accept");
+    event RideAccepted(address indexed passenger, address indexed driver);
+    event RideStarted(address indexed passenger, address indexed driver);
 
-        // Iterate through the rides array to find the ride that matches the passenger
-        for (uint256 i = 0; i < rides.length; i++) {
-            // If the ride is booked and the passenger matches the sender, accept the ride
-            if (rides[i].booked == true && rides[i].passenger == msg.sender) {
+    function acceptRide() public {
+        require(userRides[msg.sender].booked == true, "No ride booked");
+        require(
+            userRides[msg.sender].passenger == msg.sender,
+            "You cannot accept this ride"
+        );
+
+        userRides[msg.sender].booked = false;
+
+        for (uint i = 0; i < rides.length; i++) {
+            if (rides[i].passenger == msg.sender) {
                 rides[i].booked = false;
-                break;
             }
         }
+
+        emit RideAccepted(msg.sender, owner);
     }
+
+    // function acceptRide() public returns(Ride memory) {
+    //     require(msg.sender != owner, "Owner cannot accept a ride");
+    //     require(rideCount > 0, "No rides to accept");
+
+    //     // Iterate through the rides array to find the ride that matches the passenger
+    //     for (uint256 i = 0; i < rides.length; i++) {
+    //         // If the ride is booked and the passenger matches the sender, accept the ride
+    //         if (rides[i].booked == true && rides[i].passenger == msg.sender) {
+    //             rides[i].booked = false;
+    //             break;
+    //         }
+    //     }
+    //     return userRides[msg.sender];
+    // }
+
+    function startRide() public {}
 }
