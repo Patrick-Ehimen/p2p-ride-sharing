c14aa93b9d0d98fbfc2f05e6df893697ed93ef13
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-03-18 12:11:59 +0100

    Update Rides.sol

--- contracts/Rides.sol
+++ contracts/Rides.sol
@@ -1,54 +1,63 @@
-// // SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 
-// import "./P2PRide.sol";
-// import "./Drivers.sol";
+import "./P2PRide.sol";
+import "./Drivers.sol";
 
-// pragma solidity ^0.8.0;
+pragma solidity ^0.8.0;
 
-// contract Rides is P2PRide {
-//     event RideAccepted(address indexed passenger, address indexed driver);
-//     event RideStarted(address indexed passenger, address indexed driver);
+contract Rides is P2PRide {
+    event RideAccepted(
+        address indexed passenger,
+        address indexed driver,
+        block.timestamp
+    );
+    event RideStarted(address indexed passenger, address indexed driver);
 
-//     function acceptRide() public {
-//         require(userRides[msg.sender].booked == true, "No ride booked");
-//         //require(driversExist(msg.sender), "No drivers");
-//         // require(
-//         //     msg.sender == drivers[msg.sender].address,
-//         //     "Driver is not the driver"
-//         // );
-//         require(msg.sender == drivers[0].address, "Driver is not the driver");
-//         require(driversDetails[msg.sender].passengerCount > 0, "No passengers");
-//         require(driversDetails[]);
-//         require(
-//             userRides[msg.sender].passenger == msg.sender,
-//             "You cannot accept this ride"
-//         );
+    function acceptRide() public {
+        require(
+            rideAvailable == 0,
+            "No rides currently available to be accepted."
+        );
 
-//         userRides[msg.sender].booked = false;
+        // require(userRides[msg.sender].booked == true, "No ride booked");
+        // //require(driversExist(msg.sender), "No drivers");
+        // // require(
+        // //     msg.sender == drivers[msg.sender].address,
+        // //     "Driver is not the driver"
+        // // );
+        // require(msg.sender == drivers[0].address, "Driver is not the driver");
+        // require(driversDetails[msg.sender].passengerCount > 0, "No passengers");
+        // require(driversDetails[]);
+        // require(
+        //     userRides[msg.sender].passenger == msg.sender,
+        //     "You cannot accept this ride"
+        // );
 
-//         for (uint i = 0; i < rides.length; i++) {
-//             if (rides[i].passenger == msg.sender) {
-//                 rides[i].booked = false;
-//             }
-//         }
+        // userRides[msg.sender].booked = false;
 
-//         emit RideAccepted(msg.sender, owner);
-//     }
+        // for (uint i = 0; i < rides.length; i++) {
+        //     if (rides[i].passenger == msg.sender) {
+        //         rides[i].booked = false;
+        //     }
+        // }
 
-//     function cancelRide
+        // emit RideAccepted(msg.sender, owner);
+    }
 
-//     // function acceptRide() public returns(Ride memory) {
-//     //     require(msg.sender != owner, "Owner cannot accept a ride");
-//     //     require(rideCount > 0, "No rides to accept");
+    function cancelRide(string calldata _reason) public {}
 
-//     //     // Iterate through the rides array to find the ride that matches the passenger
-//     //     for (uint256 i = 0; i < rides.length; i++) {
-//     //         // If the ride is booked and the passenger matches the sender, accept the ride
-//     //         if (rides[i].booked == true && rides[i].passenger == msg.sender) {
-//     //             rides[i].booked = false;
-//     //             break;
-//     //         }
-//     //     }
-//     //     return userRides[msg.sender];
-//     // }
-// }
+    // function acceptRide() public returns(Ride memory) {
+    //     require(msg.sender != owner, "Owner cannot accept a ride");
+    //     require(rideCount > 0, "No rides to accept");
+
+    //     // Iterate through the rides array to find the ride that matches the passenger
+    //     for (uint256 i = 0; i < rides.length; i++) {
+    //         // If the ride is booked and the passenger matches the sender, accept the ride
+    //         if (rides[i].booked == true && rides[i].passenger == msg.sender) {
+    //             rides[i].booked = false;
+    //             break;
+    //         }
+    //     }
+    //     return userRides[msg.sender];
+    // }
+}
